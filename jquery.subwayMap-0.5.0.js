8f7ed1c2d6932ff56290e7db2081cda341f87600
Jon Burrows <git-hub@jonburrows.co.uk>
2016-02-25 16:52:10 +0000

    Added Modified copyright notice

--- jquery.subwayMap-0.5.0.js
+++ jquery.subwayMap-0.5.0.js
@@ -2,6 +2,8 @@
 
 Copyright (c) 2010 Nik Kalyani nik@kalyani.com http://www.kalyani.com
 
+Modified work Copyright (c) 2016 Jon Burrows subwaymap@jonburrows.co.uk https://jonburrows.co.uk
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
